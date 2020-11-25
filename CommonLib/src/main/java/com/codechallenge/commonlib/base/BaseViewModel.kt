c648993847329918dbd87b15629d55aa6eb1ad68
ftanios <tanios.fadi@gmail.com>
2020-11-25 15:50:28 +0200

    CI

--- CommonLib/src/main/java/com/codechallenge/commonlib/base/BaseViewModel.kt
+++ CommonLib/src/main/java/com/codechallenge/commonlib/base/BaseViewModel.kt
@@ -1,10 +1,5 @@
 package com.codechallenge.commonlib.base
 
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.cancel
-import kotlin.coroutines.CoroutineContext
 
 abstract class BaseViewModel : ViewModel()
